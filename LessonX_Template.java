34214f1583abf293c1cc5e1b6b2a37cefd6f8493
Ian-Chan-888 <iyc122@gmail.com>
2019-10-01 18:25:36 -0600

    Added LessonX_Template

---
+++ LessonX_Template.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
+import com.google.common.io.Files;
+
+
+public class LessonX_Template {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+
+		WebDriver driver;
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\eclipse\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		driver.manage().window().maximize();// max browser window
+		driver.manage().deleteAllCookies();
+
+		/*
+		 * 
+		 * 
+		 *
+		 * 
+		 */
+		
+		
+
+		System.out.println("END PROGRAM");
+
+		// driver.close();
+
+	}
+	
+	
+	
+	
+	
+}
