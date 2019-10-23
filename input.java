04f185bf3dc1c72997ed72894becd24b98f32d13
Ian-Chan-888 <iyc122@gmail.com>
2019-10-23 15:07:57 -0600

    updating Input and adding Lession 93-98

---
+++ input.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		
+		int n = reader.nextInt();
+				
+		int result = n;
+				for(int i=n;i>1;i--)
+				{
+					result=result*(i-1);
+				}
+		System.out.println("The Factorial is: " + result);
+		
+	}
+
+}
